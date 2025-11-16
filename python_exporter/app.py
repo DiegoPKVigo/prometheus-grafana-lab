24774f22aa4c5fa68442412537b24932d9b838f3
Diego <diego_pkv@hotmail.com>
2025-11-17 00:42:20 +0100

    Add version metric

--- python_exporter/app.py
+++ python_exporter/app.py
@@ -1,10 +1,11 @@
-from prometheus_client import start_http_server, Summary, Counter
+from prometheus_client import start_http_server, Summary, Counter, Info
 import random
 import time
 
 # Create metrics to track processing time and request counts.
 REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
 REQUEST_COUNT = Counter('request_count', 'Total number of requests')
+VERSION = Info('app_version', 'Application version')
 
 @REQUEST_TIME.time()
 def process_request(t):
@@ -14,5 +15,7 @@ def process_request(t):
 if __name__ == '__main__':
   # Expose metrics at port 8000
   start_http_server(8000)
+  VERSION.info({'version': '1.0.0'})
+
   while True:
     process_request(random.random())
